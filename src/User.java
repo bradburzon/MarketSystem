6830db3efa2909a63c2011e4e501506e0c2d12b8
Brad Burzon <bradburzon@ymail.com>
2021-03-05 16:56:29 -0500

    added setters and getter

--- src/User.java
+++ src/User.java
@@ -5,16 +5,19 @@ public class User {
 	private String lastName;
 	private String email;
 	private String password;
-	static int userID = 0; //need to make it that userID keeps being added to when making new User
+	private int userID; //need to make it that userID keeps being added to when making new User
+	static int numberOfUsers = 0;
 	private boolean loggedIn;
+
 
 	public User () {
 		this.firstName = "John";
 		this.lastName = "Doe";
 		this.email = "JohnDoe2020@email.com";
 		this.password = "password";
-		userID = 0;
 		this.loggedIn = false;
+		userID = numberOfUsers;
+		User.numberOfUsers++;
 	}
 
 	public User (String firstName, String lastName, String email, String password) {
@@ -22,8 +25,9 @@ public class User {
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
-		User.userID = userID + 1;
 		this.loggedIn = false;
+		userID = numberOfUsers;
+		User.numberOfUsers++;
 	}
 
 	public String toString() {
@@ -33,8 +37,56 @@ public class User {
 				loggedIn;
 	}
 
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+		
+	}
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public boolean isLoggedIn() {
+		return loggedIn;
+	}
+
+	public void setLoggedIn(boolean loggedIn) {
+		this.loggedIn = loggedIn;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
 	public boolean logIn(String password) {
-		loggedIn = this.password == password;
+		loggedIn = this.password.equals(password);
 
 		return loggedIn;
 	};
