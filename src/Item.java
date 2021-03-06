ef70b8cb9cb4b078989b5fc2135f6cb1b894dc19
Brad Burzon <bradburzon@ymail.com>
2021-03-06 12:23:42 -0500

    added attributes and getter/setters

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,79 @@
 
 public class Item {
+	
+	private String name;
+	private int price;
+	private int quantity;
+	private String description;
+	private int itemID;
+	static int numberOfItems = 0;
+	
+	public Item() {
+		price = 0;
+		quantity = 0;
+		description = "test";
+		itemID = numberOfItems;
+		numberOfItems++;
+	}
+	
+	public Item(String name, int price, int quantity, String description) {
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+		this.description = description;
+		itemID = numberOfItems;
+		numberOfItems++;
+	}
 
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(int itemID) {
+		this.itemID = itemID;
+	}
+
+	public static int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public static void setNumberOfItems(int numberOfItems) {
+		Item.numberOfItems = numberOfItems;
+	}
+
+	
+	
 }
---
+++ src/ItemContainer.java
@@ -0,0 +1,47 @@
+import java.util.HashSet;
+
+public class ItemContainer {
+
+	private HashSet<Item> container;
+	private int totalPrice;
+	private int numberOfItems;
+	
+	
+	public ItemContainer() {
+		container = new HashSet<Item>();
+		totalPrice = 0;
+		numberOfItems = 0;
+	}
+	
+	public ItemContainer(HashSet<Item> container, int totalPrice) {
+		this.container = container;
+		this.totalPrice = totalPrice;
+		numberOfItems = container.size();
+	}
+
+	public HashSet<Item> getContainer() {
+		return container;
+	}
+
+	public void setContainer(HashSet<Item> container) {
+		this.container = container;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public int getNumberOfItems() {
+		return numberOfItems;
+	}
+
+	public void setNumberOfItems(int numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+	
+	
+}
